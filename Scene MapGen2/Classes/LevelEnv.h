f7e9ba390783dd5152f5fce5ee9577c751e349ea
coming1024 <57028337+coming1024@users.noreply.github.com>
2020-06-21 18:24:25 +0800

    Update LevelEnv.h

--- Scene MapGen2/Classes/LevelEnv.h
+++ Scene MapGen2/Classes/LevelEnv.h
@@ -1,9 +1,9 @@
 #if true
 
 /*
-	Program LevelEnv πÿø®≥°æ∞
-	File version alpha 0.5
-	TC202006201600
+	Program LevelEnv ÂÖ≥Âç°Âú∫ÊôØ
+	File version alpha 0.3
+	TC202006192221
 	ERR=ETH (P.Q.)
 */
 
@@ -12,6 +12,13 @@
 #include <ctime>
 #include "MapGenMerge.h"
 #include "MapGenCocos.h"
+#include"qishi.h"
+#include"Chest.h"
+#include"Prop.h"
+#include"HerosHomeScene.h"
+#include"PauseLayer.h"
+#include"Monster.h"
+#include"MonsterBullet.h"
 
 class LevelEnv : public cocos2d::Scene
 {
@@ -20,6 +27,33 @@ private:
 	cocos2d::Vec2 Origin;
 	const char* Audio;
 	const char* AudioSwitch;
+	/////////////////
+	Monster* monster1;
+	Monster* monster2;
+	MonsterBullet* monsterBu1;
+	qishi* hero_qishi;
+	void initDate();
+	void onKeyPressed(EventKeyboard::KeyCode keycode, Event* event);
+	void onKeyReleased(EventKeyboard::KeyCode keycode, Event* event);
+	bool onTouchBegan(Touch* pTouch, Event* pEvent);
+	void onTouchMoved(Touch* pTouch, Event* pEvent);
+	void onTouchEnded(Touch* pTouch, Event* pEvent);
+	void upDataHeroNature(float t);
+	void upDataAddEnergy(float t);
+	bool isCollision(CCRect rect1, CCRect rect2);
+	void playerAndWeaponCollision(CCArray* weaponArray1);
+	void playerAndPropCollison(CCArray* propArray1);
+	void playerAndChestCollison(CCArray* chestArray1);
+	void monsterAndPlayerCollision(CCArray* monsterArray1);
+	void upDataMAPC(float t);
+	void monsterAndBulletCollision(CCArray* monsterArray1, CCArray* bulletArray2);
+	void upDataMABC(float t);
+	void monsterAndWeaponCollision(CCArray* monsterArray1);
+	void upDataMAWC(float t);
+	void obstacleAndBulletCollison(CCArray* barrierArray1, CCArray* bulletArray1);
+	void upDataOABC(float t);
+	void upDataDeath(float t);
+	void BackToHerosHomeScene(cocos2d::Ref* pSender);
 public:
 	LevelEnv();
 	~LevelEnv();
@@ -29,10 +63,23 @@ public:
 	MGM::Cmap* GlobalMap;
 	Node* GenerateEnv(int level);
 	void Trigger(Ref* pSender);
+
+	/// <summary>
+	/// ////////////////
+	/// </summary>
+	CCArray* weaponArray;
+	CCArray* propArray;
+	CCArray* chestArray;
+	CCArray* monsterArray;///
+	SpriteFrameCache* equipmentCache;
+	SpriteFrameCache* bulletCache;
+	SpriteFrameCache* propCache;
+	SpriteFrameCache* chestCache;
+	SpriteFrameCache* heroCache;
 };
 
 /*
-	Program LevelEnv πÿø®≥°æ∞
+	Program LevelEnv ÂÖ≥Âç°Âú∫ÊôØ
 	End
 */
 
